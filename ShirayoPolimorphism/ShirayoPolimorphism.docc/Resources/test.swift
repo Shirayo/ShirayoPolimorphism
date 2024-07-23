f6ccbb82d02176734ffebb2b70e151af1d86a5d8
Shirayo <iphoje@gmail.com>
2024-07-23 14:08:13 +0300

    test commit

---
+++ ShirayoPolimorphism/ShirayoPolimorphism.docc/Resources/test.swift
@@ -0,0 +1,16 @@
+//
+//  test.swift
+//  
+//
+//  Created by Apple on 23/07/2024.
+//
+
+import Foundation
+import SwiftUI
+
+public struct ShirayoView: View {
+    var body: some View {
+        Text("hello world")
+            .foregroundStyle(.red)
+    }
+}
