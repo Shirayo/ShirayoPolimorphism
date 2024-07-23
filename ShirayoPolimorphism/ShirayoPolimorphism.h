dda94a04f402cbf5683dfd1b27d850958ffe9041
Shirayo <iphoje@gmail.com>
2024-07-23 14:00:44 +0300

    Initial Commit

---
+++ ShirayoPolimorphism/ShirayoPolimorphism.h
@@ -0,0 +1,18 @@
+//
+//  ShirayoPolimorphism.h
+//  ShirayoPolimorphism
+//
+//  Created by Apple on 23/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShirayoPolimorphism.
+FOUNDATION_EXPORT double ShirayoPolimorphismVersionNumber;
+
+//! Project version string for ShirayoPolimorphism.
+FOUNDATION_EXPORT const unsigned char ShirayoPolimorphismVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShirayoPolimorphism/PublicHeader.h>
+
+
